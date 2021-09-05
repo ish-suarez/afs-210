3fcf117d91d962abff56db7bc78f7112c4d47ab7
ish-suarez <ish-suarez@outlook.com>
2021-09-05 11:15:34 -0500

    comments added

--- week1/List/List.py
+++ week1/List/List.py
@@ -1,8 +1,13 @@
-class List():
-    def __init__(self) -> None:
-        self.numbers = []
-        pass
 
+# Class for list
+class List():
+
+    #  Init list of numbers
+    def __init__(self) -> None:
+        self.numbers = [],
+        pass
+    
+    # Add a series of numbers into list, and sort them.
     def add_num(self):
         for i in range(10): 
             num = int(input(f'Enter a number into the list: '))
@@ -10,6 +15,9 @@ class List():
             self.numbers.sort()
         
 
+# Setting list variable and calling input function
 list = List()
 list.add_num()
+
+# Printing the sorted list
 print(f'This is your sorted list from lowest to highest: {list.numbers}')
